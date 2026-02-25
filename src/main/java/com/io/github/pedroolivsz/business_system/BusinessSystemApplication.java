5ccab98cbf723af5510316ae2ca3b6dd16f48823
João Pedro <joaopedropereira2209@gmail.com>
2026-02-25 10:26:09 -0300

    Initial commit

---
+++ src/main/java/com/io/github/pedroolivsz/business_system/BusinessSystemApplication.java
@@ -0,0 +1,13 @@
+package com.io.github.pedroolivsz.business_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinessSystemApplication.class, args);
+	}
+
+}
